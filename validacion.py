3bdedf38c550527ae2e11db361e7dfa24988a9e9
jfanara <124005769+Johanna-Fanara@users.noreply.github.com>
2024-09-16 11:41:18 -0300

    revisión de la explicaciones

--- validacion.py
+++ validacion.py
@@ -4,7 +4,10 @@ import datetime
 import re
 
 def validar_fecha(fecha_str):
-    """ Valida que la fecha sea un formato correcto y no sea una fecha futura """
+    """ 
+Pre: fecha_str debe ser una cadena de caracteres en el formato 'YYYY-MM-DD'. El módulo datetime tiene que estar importado.
+Pos: La función valida que la fecha esté en el formato correcto y que no sea una fecha futura. Si la validación es exitosa, retorna el objeto date correspondiente a la fecha. Si la fecha es incorrecta, imprime un mensaje de error.  
+    """
     try:
         fecha = datetime.datetime.strptime(fecha_str, '%Y-%m-%d').date()
         if fecha > datetime.date.today():
@@ -16,5 +19,9 @@ def validar_fecha(fecha_str):
     
 
 def validar_email(email):
+  """
+Pre: email tiene que ser una cadena de caracteres. El módulo re tiene que estar importado.
+Pos: La función valida si la cadena email tiene el formato de una dirección de correo electrónico. Retorna True si el formato es correcto según el patrón de la expresión regular, y False si no lo es.
+  """
     patron = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(patron, email) is not None
